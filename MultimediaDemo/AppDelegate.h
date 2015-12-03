a2dbf54a73009dc41a2c3c8fc4ef5c25076c1a59
ruanyanchao <756583873@qq.com>
2015-12-03 10:10:03 +0800

    Initial Commit

---
+++ MultimediaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultimediaDemo
+//
+//  Created by Ryan on 15/12/3.
+//  Copyright © 2015年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
